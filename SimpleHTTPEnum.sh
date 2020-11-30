ac6a7c93c4f52304ab8c876dddcad205932929ec
Nicholas Newman <nick.newman95@yahoo.com>
2020-11-30 15:40:55 -0500

    Update SimpleHTTPEnum.sh

--- SimpleHTTPEnum.sh
+++ SimpleHTTPEnum.sh
@@ -16,17 +16,17 @@ echo "[+] Created TargetURL/recon/assetFinder.txt..."
 cat $url/recon/assetFinder.txt | grep $1 >> $url/recon/assetFinder_SubDomains.txt
 echo "[+] Created TargetURL/recon/assetFinder_SubDomains.txt..."
 
-#echo "[+] Harvesting subdomains with Amass..."
-#amass enum -d $url >> $url/recon/aMass.txt
-#echo "[+] Created TargetURL/recon/aMass.txt..."
-#sort -u $url/recon/aMass.txt >> $url/recon/aMass_SubDomains.txt
-#echo "[+] Created TargetURL/recon/aMass_SubDomains.txt..."
+echo "[+] Harvesting subdomains with Amass..."
+amass enum -d $url >> $url/recon/aMass.txt
+echo "[+] Created TargetURL/recon/aMass.txt..."
+sort -u $url/recon/aMass.txt >> $url/recon/aMass_SubDomains.txt
+echo "[+] Created TargetURL/recon/aMass_SubDomains.txt..."
 
 echo "[+] Probing for alive domains with assestFinder_SubDomains.txt..."
 cat $url/recon/assetFinder_SubDomains.txt | sort -u | httprobe >> $url/recon/alive_assetFinder_SubDomains.txt
 echo "[+] Created TargetURL/recon/alive_assetFinder_SubDomains.txt..."
 
-#echo "[+] Probing for alive domains with aMass_SubDomains.txt..."
-#cat $url/recon/aMass_SubDomains.txt | sort -u | httprobe >> $url/recon/alive_aMass_SubDomains.txt
-#echo "[+] Created TargetURL/recon/alive_aMass_SubDomains.txt..."
+echo "[+] Probing for alive domains with aMass_SubDomains.txt..."
+cat $url/recon/aMass_SubDomains.txt | sort -u | httprobe >> $url/recon/alive_aMass_SubDomains.txt
+echo "[+] Created TargetURL/recon/alive_aMass_SubDomains.txt..."
 
